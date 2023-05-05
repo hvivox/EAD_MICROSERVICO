549a39381c2bb1aca076b38ee82a4db81f8aa0e3
hvivox <hvivox@hotmail.com>
2023-05-05 09:31:54 -0400

    API Composition Pattern em Authuser Microservice – Parte 01

---
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -0,0 +1,51 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class CourseClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    UtilsService utilsService;
+
+    @Value("${ead.api.url.course}")
+    String REQUEST_URL_COURSE;
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+        List<CourseDto> searchResult = null;
+        String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+        try{
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Response Number of Elements: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e){
+            log.error("Error request /courses {} ", e);
+        }
+        log.info("Ending request /courses userId {} ", userId);
+        return new PageImpl<>(searchResult);
+    }
+}
