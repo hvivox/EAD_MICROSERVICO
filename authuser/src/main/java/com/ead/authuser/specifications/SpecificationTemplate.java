6b87612569523eedcaffd3b6d5c65ee705cbcf33
hvivox <hvivox@hotmail.com>
2023-05-30 15:07:32 -0400

    CRIAR SERVICE REGISTRY COM SPRING CLOUD NETFLIX EUREKA

--- authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -4,6 +4,7 @@ import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,7 +18,7 @@ public class SpecificationTemplate {
         @Spec(path = "userType", spec = Equal.class),
         @Spec(path = "userStatus", spec = Equal.class),
         @Spec(path = "email", spec = Like.class),
-        @Spec(path = "fullName", spec = Like.class)
+        @Spec(path = "fullName", spec =  LikeIgnoreCase.class )
     })
     public interface UserSpec extends Specification<UserModel> {}
 
